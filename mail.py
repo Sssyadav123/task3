eff677512e4313f5e888530159ae65a2910e0fd3
Sssyadav123 <64545384+Sssyadav123@users.noreply.github.com>
2020-07-02 10:39:57 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,21 @@
+# Python code to illustrate Sending mail from  
+# your Gmail account  
+import smtplib 
+  
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+# start TLS for security 
+s.starttls() 
+  
+# Authentication 
+s.login("sumityadav25199@gmail.com", "sssyadav") 
+  
+# message to be sent 
+message = "This mail is notify the developer hat your latest code been failed ,please take action on it as soon as possibe"
+  
+# sending the mail 
+s.sendmail("sumityadav25199@gmail.com", "sumityadav25199@gmail.com", message1) 
+  
+# terminating the session 
+s.quit() 
