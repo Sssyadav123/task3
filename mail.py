34d75fba8ef9614ce84b8c6a1803db25a8c422d3
Sssyadav123 <64545384+Sssyadav123@users.noreply.github.com>
2020-07-02 11:13:26 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,21 +1,7 @@
-# Python code to illustrate Sending mail from  
-# your Gmail account  
-import smtplib 
-  
-# creates SMTP session 
-s = smtplib.SMTP('smtp.gmail.com', 587) 
-  
-# start TLS for security 
+ import smtplib 
+s = smtplib.SMTP('smtp.gmail.com', 587)  
 s.starttls() 
-  
-# Authentication 
 s.login("sumityadav25199@gmail.com", "ssssyadav") 
-  
-# message to be sent 
 message = "This mail is notify the developer hat your latest code been failed ,please take action on it as soon as possibe"
-  
-# sending the mail 
 s.sendmail("sumityadav25199@gmail.com", "sumityadav25199@gmail.com", message1) 
-  
-# terminating the session 
 s.quit() 
