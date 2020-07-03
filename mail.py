192121813c0f540aef996afa5e1b9b915aebed28
Sssyadav123 <64545384+Sssyadav123@users.noreply.github.com>
2020-07-03 13:33:21 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,7 +1,7 @@
 import smtplib 
 s = smtplib.SMTP('smtp.gmail.com', 587)  
 s.starttls() 
-s.login("sumityadav25199@gmail.com", "ssssyadav") 
+s.login("sumityadav25199@gmail.com", "ss*******") 
 message = "This mail is notify the developer hat your latest code been failed ,please take action on it as soon as possibe"
 s.sendmail("sumityadav25199@gmail.com", "sumityadav25199@gmail.com", message1) 
 s.quit() 
